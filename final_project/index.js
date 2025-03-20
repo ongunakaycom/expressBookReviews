399c2da91502ed7d053410b66184468e8004bd2c
Ongun <info@ongunakay.com>
2025-03-21 00:37:51 +0300

    commit

--- final_project/index.js
+++ final_project/index.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const { books, getBooksByAuthor, getBooksByTitle, getReviewsByBookId, getReviewsByTitle } = require('./router/booksdb'); // Import books module
 
 const app = express();
 
@@ -17,6 +18,54 @@ app.use("/customer", session({ secret: "fingerprint_customer", resave: true, sav
 app.use("/customer/auth/*", function auth(req, res, next) {
     // Write the authentication mechanism here
     next();
+});
+
+// New route to get books by author
+app.get('/books/author/:name', (req, res) => {
+    const authorName = req.params.name;
+    const authorBooks = getBooksByAuthor(authorName);
+    
+    if (authorBooks.length > 0) {
+        res.json(authorBooks);
+    } else {
+        res.status(404).json({ message: 'No books found for this author.' });
+    }
+});
+
+// New route to get books by title
+app.get('/books/title/:title', (req, res) => {
+    const title = req.params.title;
+    const titleBooks = getBooksByTitle(title);
+    
+    if (titleBooks.length > 0) {
+        res.json(titleBooks);
+    } else {
+        res.status(404).json({ message: 'No books found with this title.' });
+    }
+});
+
+// New route to get reviews by book ID
+app.get('/books/reviews/:id', (req, res) => {
+    const bookId = req.params.id;
+    const reviews = getReviewsByBookId(bookId);
+    
+    if (reviews) {
+        res.json(reviews);
+    } else {
+        res.status(404).json({ message: 'No reviews found for this book.' });
+    }
+});
+
+// New route to get reviews by book title
+app.get('/books/reviews/title/:title', (req, res) => {
+    const title = req.params.title;
+    const reviews = getReviewsByTitle(title);
+    
+    if (reviews) {
+        res.json(reviews);
+    } else {
+        res.status(404).json({ message: 'No reviews found for this book title.' });
+    }
 });
 
 const PORT = 5000;
